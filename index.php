270c466aef242eca7d4ad86dd4e5eec3f6525bd2
El-NyaX3M <niggel.x3m@gmail.com>
2023-12-14 13:54:11 -0700

    botón whatsapp

--- index.php
+++ index.php
@@ -31,8 +31,9 @@ include 'lang/' . $userLanguage . '.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+   
     <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 
     <title><?php echo translate('title')?></title>
 </head>
@@ -49,6 +50,12 @@ include 'lang/' . $userLanguage . '.php';
     </a>
   </div>
   <!-- BOTÓN BOLETOS END -->
+
+  <!-- BOTÓN WHATSAPP -->
+    <a href="https://wa.me/5216122210135" class="floating" target="_blank">
+      <i class="fab fa-whatsapp fab-icon"></i>
+    </a>
+  <!-- BOTÓN WHATSAPP END -->
 
   <!-- INICIO / HEADER -->
   <div id="inicio" class="container-fluid section-wall-bg px-0 pt-0">
@@ -84,9 +91,11 @@ include 'lang/' . $userLanguage . '.php';
             <div class="col">
               <a href="#jardin-secreto" class="link-light link-underline link-underline-opacity-0"><p class="h4"><?php echo translate('section-garden') ?></p></a>
             </div>
+<!-- 
             <div class="col">
               <a href="#calendario" class="link-light link-underline link-underline-opacity-0"><p class="h4"><?php echo translate('section-events') ?></p></a>
             </div>
+-->
             <div class="col">
               <a href="#grupos" class="link-light link-underline link-underline-opacity-0"><p class="h4"><?php echo translate('section-groups') ?></p></a>
             </div>
@@ -108,7 +117,7 @@ include 'lang/' . $userLanguage . '.php';
                   <li><a class="dropdown-item" href="#tu-visita"><?php echo translate('section-visit') ?></a></li>
                   <li><a class="dropdown-item" href="#boletos"><?php echo translate('section-tickets') ?></a></li>
                   <li><a href="#jardin-secreto" class="dropdown-item"><?php echo translate('section-garden') ?></a></li>
-                  <li><a href="#calendario" class="dropdown-item"><?php echo translate('section-events') ?></a></li>
+                  <!-- <li><a href="#calendario" class="dropdown-item"><?php echo translate('section-events') ?></a></li> -->
                   <li><a class="dropdown-item" href="#grupos"><?php echo translate('section-groups') ?></a></li>
                   <li><a class="dropdown-item" href="#servicios"><?php echo translate('section-services') ?></a></li>
                   <li>
@@ -418,7 +427,7 @@ include 'lang/' . $userLanguage . '.php';
   </div>
   <!-- SLIDER END -->
 
-  <!-- CALENDARIO -->
+  <!-- CALENDARIO 
   <div id="calendario" class="container-fluid px-0 bg-black calendario">
 
     <div class="container-fluid">
@@ -495,7 +504,7 @@ include 'lang/' . $userLanguage . '.php';
       </div>
     </div>
   </div>
-  <!-- CALENDARIO END -->
+  CALENDARIO END -->
 
   <!-- GRUPOS -->
   <div id="grupos" class="container-fluid px-0 bg-white">
@@ -902,110 +911,8 @@ include 'lang/' . $userLanguage . '.php';
     </div>
   </div>
   <!-- Extras 1 END -->
-  <!-- MODAL AVISO DE PRIVACIDAD -->
-
   
-  
-  <!-- Extras 2 
-  <div class="container-fluid px-0 pb-0" style="background-color: var(--color-1);">
-    <div class="container">
-      <div class="row text-center">
-        <div class="col">
-          <!-- Button trigger modal
-          <button type="button" class="btn btn-primary no-button-style p-0" data-bs-toggle="modal" data-bs-target="#avisoPrivacidad">
-            <p class="white-poppins mt-3" style="font-size: 14px;">Aviso de Privacidad</p>
-          </button>
 
-          <!-- Modal Aviso de Privacidad
-          <div class="modal fade" id="avisoPrivacidad" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Aviso de Privacidad</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body" style="text-align: start;">
-                  <p>FRIDA EN SU JARDIN SECRETO <br><br>
-                    Francisco I. Madero #140, Col. Centro. La Paz, Baja California Sur <br><br>
-                    FRIDA EN SU JARDIN SECRETO con domicilio en Francisco I. Madero #140, Col. Centro, es el responsable del uso y protección de sus datos personales, y al respecto le informa lo siguiente: <br><br>
-                    Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que no son necesarias para el servicio solicitado pero que nos permiten y facilitan brindarle una mejor atención: <br><br>
-                    Ofrecer contenidos personalizados; promociones y noticias exclusivas sobre los eventos de FRIDA EN SU JARDIN SECRETO relacionados con sus intereses. Anunciar preventas, descuentos y cambios de este aviso de privacidad. <br><br>  
-                    FRIDA EN SU JARDIN SECRETO no transfiere a ningún tercero los Datos personales que recabe en sitio y/o otro medio. <br><br>
-                    La información recabada podrá ser utilizada para fines mercadológicos y de prospección comercial como conducir investigaciones mercadológicas o promocionales. <br><br>
-                    Derechos ARCO <br><br>
-                    Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada conforme a los principios, deberes y obligaciones previstas en la normativa (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO. <br><br>
-                    Para el ejercicio de cualquiera de los derechos ARCO, usted deberá presentar la solicitud respectiva enviar un mail a contacto@fridaensujardinsecreto.com  con la siguiente información: <br><br>
-                    Copia electrónica de identificación oficial. <br><br>
-                    Descripción de los datos sobre los que quisiera información e indicar las modificaciones a realizarse (en caso de requerirlas). <br><br>
-                    En caso de que no desee que sus datos personales sean tratados para estos fines adicionales, desde este momento usted nos puede comunicar lo anterior, notificando a nuestro personal de taquilla o mandando un mail a contacto@fridaensujardinsecreto.com <br><br>
-                    La negativa para el uso de sus datos personales para estas finalidades no podrá ser un motivo para que le neguemos los servicios y productos que solicita o contrata con nosotros. <br><br>
-                    FRIDA EN SU JARDIN SECRETO notificará cualquier cambio en el Aviso de Privacidad al correo registrado del usuario.
-                  </p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- MODAL AVISO DE PRIVACIDAD END
-        </div>
-
-        <div class="col">
-          <!-- Button trigger modal
-          <button type="button" class="btn btn-primary no-button-style p-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <p class="white-poppins mt-3" style="font-size: 14px;">Términos de Uso</p>
-          </button>
-
-          <!-- Modal Térmios de Uso
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Términos de Uso</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body" style="text-align: start;">
-                  <p>
-                    FRIDA EN SU JARDÍN SECRETO <br><br>
-                    Gracias por comprar en FRIDA EN SU JARDÍN SECRETO. <br>
-                    Por favor tome nota de los siguientes: <br><br>
-                     
-                    
-                    <b>TÉRMINOS DE USO</b><br><br>
-                     
-                    <b>1.</b>  Todas las ventas que se hagan por el sistema son definitivas, NO se aceptan: cancelaciones de compra, cambios o reembolsos. <br><br>
-                     
-                    <b>2.</b> FRIDA EN SU JARDIN SECRETO no envía boletos a domicilio. Los horarios de atención en taquilla son: <br><br>
-                    
-                    
-                    
-                    Lunes a sábado <br>
-                    8 am - 8 pm <br><br>
-                    Domingos <br>
-                    8 am – 6 pm <br><br>
-                    
-                    <b>3.</b> En caso de que un HORARIO sea cancelado o pospuesto, FRIDA EN SU JARDIN SECRETO hará todo el esfuerzo para contactar vía e-mail a cada cliente en el plazo permitido. <br><br>
-                    <b>4.</b> Mientras FRIDA EN SU JARDIN SECRETO lamenta los inconvenientes causados por estas circunstancias, FRIDA EN SU JARDIN SECRETO no se hará responsable de ningún costo o daño que pueda ocasionar tal cancelación o aplazamiento en los horarios. Semejantemente, la realización de horarios para boletos vendidos a los clientes es de completa responsabilidad de los operadores del recinto, FRIDA EN SU JARDIN SECRETO no será responsable de las pérdidas o daños causados a los clientes de estos eventos. <br><br>
-                    
-                    <b>5.</b> Todos los clientes que compran por FRIDA EN SU JARDIN SECRERO dan conocimiento de las limitaciones expuestas en estos “términos de uso”. <br><br>
-                     
-                    <b>6.</b> Los reembolsos sólo se realizarán para los horarios cancelados. En todos los casos cuando un boleto es reembolsado, sólo se reembolsa el costo del boleto, sin cargos.
-                  </p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- MODAL TÉRMINOS DE USO END
-        </div>
-      </div>
-    </div>
-  </div>
-  Extras 2 END -->
-  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   
   <!-- Google tag (gtag.js) -->
